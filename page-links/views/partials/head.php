331681897032f5a037214f0ddb408280ca4f4cbe
sourav173 <sourav.sardar@myyahoo.com>
2025-07-31 20:56:01 +0530

    page linking

---
+++ page-links/views/partials/head.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script defer src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body>
+  <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+  <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
+  <!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+  <div class="min-h-full">
